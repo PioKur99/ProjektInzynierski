b1e48ab6beb2c3ea3a303a752f4d4260be644714
Piokur99 <piotrkurda99@interia.pl>
2021-07-04 09:54:46 +0200

    Firebase setup and read/write test went good

--- FitManager/FitManager/Controllers/SupperViewController.swift
+++ FitManager/FitManager/Controllers/SupperViewController.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
-
+import FirebaseDatabase
 class SupperViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let DB = Database.database(url: "https://fitmanager-database-default-rtdb.europe-west1.firebasedatabase.app").reference()
+        DB.child("Test").observeSingleEvent(of: .value, with: {snapshot in
+            guard let value = snapshot.value as? [String: String] else {
+                return
+            }
+            print("Value: \(value)" )
+        })
+        
 
     }
 
--- FitManager/FitManager/Controllers/TabBarViewController.swift
+++ FitManager/FitManager/Controllers/TabBarViewController.swift
@@ -6,11 +6,19 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class TabBarViewController: UITabBarController {
     
+    let DB = Database.database(url: "https://fitmanager-database-default-rtdb.europe-west1.firebasedatabase.app").reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let testObj: [String: String] = [
+            "name" : "Piotr",
+            "lastName": "K"
+        ]
+        DB.child("Test").setValue(testObj)
         self.selectedIndex = 1
     }
     
