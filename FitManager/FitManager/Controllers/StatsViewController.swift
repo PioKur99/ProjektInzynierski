aae7945651ffe96b2245c0cc54e987bb2cdb032b
Piokur99 <piotrkurda99@interia.pl>
2021-08-06 18:21:57 +0200

    Added view for calories history

---
+++ FitManager/FitManager/Controllers/StatsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StatsViewController.swift
+//  FitManager
+//
+//  Created by Piotr Kurda on 06/08/2021.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
