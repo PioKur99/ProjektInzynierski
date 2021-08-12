e3f48527eeaf35491892622c5967d685416cace7
Piokur99 <piotrkurda99@interia.pl>
2021-08-12 09:25:45 +0200

    Chart setup in stats section

--- FitManager/FitManager/Controllers/StatsViewController.swift
+++ FitManager/FitManager/Controllers/StatsViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import Charts
 
-class StatsViewController: UIViewController {
-
+class StatsViewController: UIViewController, ChartViewDelegate {
+    
+    var statsChart = BarChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        statsChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        statsChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        statsChart.center = view.center
+        view.addSubview(statsChart)
+        let set = BarChartDataSet(entries: [
+            BarChartDataEntry(x: 10.0, y: 20.0),
+            BarChartDataEntry(x: 15.0, y: 20.0),
+            BarChartDataEntry(x: 20.0, y: 20.0),
+        ])
+        let data = BarChartData(dataSet: set)
+        statsChart.data = data
     }
 
 }
