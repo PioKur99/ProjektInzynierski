e6e9781d2b9508e66195850356afc3fb58516b66
Piokur99 <piotrkurda99@interia.pl>
2021-08-13 14:06:53 +0200

    Displaying stats data on bar chart

--- FitManager/FitManager/Controllers/StatsViewController.swift
+++ FitManager/FitManager/Controllers/StatsViewController.swift
@@ -13,23 +13,22 @@ class StatsViewController: UIViewController {
     
     var chartView: BarsChart!
     var statsData: [HistoryItem] = []
+    var barsArray: [(String, Double)] = []
     let DB = Database.database(url: "https://fitmanager-database-default-rtdb.europe-west1.firebasedatabase.app").reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setUpChart()
         initChartData()
     }
     
-    func updateChartData() {
-
-    }
-    
     func setUpChart() {
+        barsArray.removeAll()
         let chartConfig = BarsChartConfig(valsAxisConfig: ChartAxisConfig(from: 0, to: 4000, by: 500))
-        let sFrame = CGRect(x: 0, y: 130, width: self.view.frame.width, height: self.view.frame.width)
-        let barsArray = [(statsData[0].date,statsData[0].caloriesAmount),("Feb",110.8),("Mar",200),("Apr",310.6),("May",200),("Jun",410.5),("Jul",300)]
-        let chart = BarsChart(frame: sFrame, chartConfig: chartConfig, xTitle: "Dzień", yTitle: "Spożyte kalorie", bars: barsArray, color: UIColor.orange, barWidth: 40)
+        let sFrame = CGRect(x: 0, y: 50, width: self.view.frame.width - 20, height: self.view.frame.width + 200)
+        for elem in statsData {
+            barsArray.append((elem.date,elem.caloriesAmount))
+        }
+        let chart = BarsChart(frame: sFrame, chartConfig: chartConfig, xTitle: "Dzień", yTitle: "Spożyte kalorie", bars: barsArray, color: UIColor.orange, barWidth: 10)
         self.view.addSubview(chart.view)
         self.chartView = chart
     }
@@ -39,11 +38,13 @@ class StatsViewController: UIViewController {
         DB.child("History").observeSingleEvent(of: .value, with: {snapshot in
             for child in snapshot.children.allObjects as! [DataSnapshot]{
                 let newEntry = HistoryItem(snapshot: child)
-                newEntry.printItem()
                 self.statsData.append(newEntry)
-                self.setUpChart()
             }
         })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.setUpChart()
+        }
     }
 
 }
